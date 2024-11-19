ab24862283ce01acdee932577dafb5f143298e16
gustavodiniz <gustavodinniz.dev@gmail.com>
2024-11-19 18:54:21 -0300

    feat: removing unused constructors for AuthorNotFoundException and InvalidUuidFormatException

--- src/main/java/com/gustavodinniz/bookstore_service/exception/InvalidUuidFormatException.java
+++ src/main/java/com/gustavodinniz/bookstore_service/exception/InvalidUuidFormatException.java
@@ -2,18 +2,9 @@ package com.gustavodinniz.bookstore_service.exception;
 
 public class InvalidUuidFormatException extends RuntimeException {
 
-
     public InvalidUuidFormatException(String message) {
         super(message);
     }
 
 
-    public InvalidUuidFormatException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-
-    public InvalidUuidFormatException(Throwable cause) {
-        super(cause);
-    }
 }
